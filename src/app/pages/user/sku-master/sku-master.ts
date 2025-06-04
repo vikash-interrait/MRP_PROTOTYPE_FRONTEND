21b4d53d4801e639c729c13b0f627e6aaa130099
archits62@gmail.com <ArchitSh@interrait.com>
2025-06-04 12:50:01 +0530

    Update sku-master.ts
    commit

--- src/app/pages/user/sku-master/sku-master.ts
+++ src/app/pages/user/sku-master/sku-master.ts
@@ -6,6 +6,62 @@ import { Component } from '@angular/core';
   templateUrl: './sku-master.html',
   styleUrl: './sku-master.css'
 })
+
 export class SkuMaster {
+
+  SKU = [
+    {
+      skuCode: "KBAFP93847",
+      productLine: "Kitchen",
+      productCategory: "Base Cabinet",
+      productFamily: "Flat Panel",
+      productModel: "Shaker",
+      dimensions: "600 x 800 x 500 mm",
+      material: "MDF",
+      finish: "White",
+      hardware: "Steel",
+      configuration: "Single Door",
+      productDescription: "Sleek white base cabinet with flat panel design, offering minimalist aesthetic and efficient storage."
+    },
+    {
+      skuCode: "KBAFP27491",
+      productLine: "Kitchen",
+      productCategory: "Base Cabinet",
+      productFamily: "Flat Panel",
+      productModel: "Shaker",
+      dimensions: "800 x 800 x 500 mm",
+      material: "MDF",
+      finish: "White",
+      hardware: "Steel",
+      configuration: "Single Door",
+      productDescription: "800mm wide base cabinet in white, featuring flat panel design that complements modern kitchen interiors."
+    },
+    {
+      skuCode: "KBAFP93847",
+      productLine: "Kitchen",
+      productCategory: "Base Cabinet",
+      productFamily: "Flat Panel",
+      productModel: "Shaker",
+      dimensions: "600 x 800 x 500 mm",
+      material: "MDF",
+      finish: "White",
+      hardware: "Steel",
+      configuration: "Single Door",
+      productDescription: "Sleek white base cabinet with flat panel design, offering minimalist aesthetic and efficient storage."
+    },
+    {
+      skuCode: "KBAFP27491",
+      productLine: "Kitchen",
+      productCategory: "Base Cabinet",
+      productFamily: "Flat Panel",
+      productModel: "Shaker",
+      dimensions: "800 x 800 x 500 mm",
+      material: "MDF",
+      finish: "White",
+      hardware: "Steel",
+      configuration: "Single Door",
+      productDescription: "800mm wide base cabinet in white, featuring flat panel design that complements modern kitchen interiors."
+    },
+  ];
 
 }
